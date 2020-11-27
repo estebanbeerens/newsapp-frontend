35832a448a7960339b7ee47af9f61e13d47acc82
Esteban Beerens <esteban.beerens@gmail.com>
2020-11-27 16:41:52 +0100

    Added filter options to articles

--- src/app/features/articles/components/articles-overview/articles-overview-presenter/articles-overview-presenter.component.ts
+++ src/app/features/articles/components/articles-overview/articles-overview-presenter/articles-overview-presenter.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DataSource } from '@angular/cdk/table';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { IArticleStatus } from 'src/app/features/article-statuses/models/entities/article-status';
 import { IArticle } from 'src/app/features/articles/models/entities/article';
 import { ITag } from 'src/app/features/tags/models/entities/tag';
 
@@ -9,15 +9,60 @@ import { ITag } from 'src/app/features/tags/models/entities/tag';
   templateUrl: './articles-overview-presenter.component.html',
   styleUrls: ['./articles-overview-presenter.component.scss']
 })
-export class ArticlesOverviewPresenterComponent {
+export class ArticlesOverviewPresenterComponent implements OnChanges {
   
   selected = new FormControl("0");
-  selectedTagId: number;
+
+  dataSource: IArticle[] = [];
   
   @Input() articles: IArticle[];
   @Input() tags: ITag[];
 
   @Output() onRefresh = new EventEmitter();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.articles) {
+      this.initDataSource(changes.articles.currentValue);
+    }
+  }
+
+  initDataSource(articles: IArticle[]): void {
+    this.dataSource = articles;
+  }
+
+  applySearchFilter(event: Event): void {
+    const filterValue: string = (event.target as HTMLInputElement).value.toLowerCase();
+    let filteredArticles: IArticle[] = [];
+
+    if (filterValue == '') {
+      this.applyRoleFilter();
+    } else {
+      this.articles.forEach((article) => {
+        if (article.title.toLowerCase().includes(filterValue) || article.shortSummary.toLowerCase().includes(filterValue)) {
+          if (article.tagID == this.selected.value || this.selected.value == 0) {
+            filteredArticles.push(article);
+          }
+        }
+      });
+      this.initDataSource(filteredArticles);
+    }
+  }
+
+  applyRoleFilter(): void {
+    let filteredArticles: IArticle[] = [];
+
+    if (this.selected.value == 0) {
+      filteredArticles = this.articles;
+    } else {
+      this.articles.forEach((article) => {
+        if (article.tagID == this.selected.value) {
+          filteredArticles.push(article);
+        }
+      });
+    }
+    
+    this.initDataSource(filteredArticles);
+  }
 
   refresh(): void {
     this.onRefresh.emit();
